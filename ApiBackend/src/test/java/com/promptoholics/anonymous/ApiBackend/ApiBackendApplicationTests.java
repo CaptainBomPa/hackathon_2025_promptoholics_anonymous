7fe2526c804dfe345cbfdeef80da7655ff50b56d
Filip Mróz <filip.mroz12@gmail.com>
2025-10-01 18:27:28 +0200

    Init repo

---
+++ ApiBackend/src/test/java/com/promptoholics/anonymous/ApiBackend/ApiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promptoholics.anonymous.ApiBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
