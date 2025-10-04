f9b711d14ecbe89c9a39c9795afbc6944039a597
misiek <mbialozyt@student.agh.edu.pl>
2025-10-04 19:13:24 +0200

    feat: update services to new open api

--- ApiBackend/src/main/java/com/promptoholics/anonymous/ApiBackend/adapters/CalculationController.java
+++ ApiBackend/src/main/java/com/promptoholics/anonymous/ApiBackend/adapters/CalculationController.java
@@ -2,8 +2,7 @@ package com.promptoholics.anonymous.ApiBackend.adapters;
 
 import com.promptoholics.anonymous.ApiBackend.api.CalculationApi;
 import com.promptoholics.anonymous.ApiBackend.application.CalculationFacade;
-import com.promptoholics.anonymous.ApiBackend.schemas.dtos.PensionCalculationRequestDto;
-import com.promptoholics.anonymous.ApiBackend.schemas.dtos.PensionCalculationResponseDto;
+import com.promptoholics.anonymous.ApiBackend.schemas.dtos.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -19,6 +18,20 @@ public class CalculationController implements CalculationApi {
     @Override
     public ResponseEntity<PensionCalculationResponseDto> calculatePensions(PensionCalculationRequestDto request) {
         var response = calculationFacade.calculatePensions(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @Override
+    public ResponseEntity<Void> enterPostalCodeForCalculation(String calculationId, PostalCodeUpdateRequestDto postalCodeUpdateRequestDto) {
+        calculationFacade.enterPostalCodeForCalculation(calculationId, postalCodeUpdateRequestDto);
+        return ResponseEntity.ok(null);
+    }
+
+    @Override
+    public ResponseEntity<ReportJobDto> generateCalculationReport(
+            String calculationId, UserReportCreateRequestDto userReportCreateRequestDto
+    ) {
+        var response = calculationFacade.generateCalculationReport(calculationId, userReportCreateRequestDto);
         return ResponseEntity.ok(response);
     }
 }
--- ApiBackend/src/main/java/com/promptoholics/anonymous/ApiBackend/application/AdministrationFacade.java
+++ ApiBackend/src/main/java/com/promptoholics/anonymous/ApiBackend/application/AdministrationFacade.java
@@ -1,22 +1,13 @@
 package com.promptoholics.anonymous.ApiBackend.application;
 
-import com.promptoholics.anonymous.ApiBackend.schemas.dtos.ReportCreateRequestDto;
+import com.promptoholics.anonymous.ApiBackend.schemas.dtos.AdminReportCreateRequestDto;
 import com.promptoholics.anonymous.ApiBackend.schemas.dtos.ReportJobDto;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AdministrationFacade {
 
-    public Resource downloadAdminReport(String reportId) {
-        return null;
-    }
-
-    public ReportJobDto generateAdminReport(ReportCreateRequestDto reportCreateRequestDto) {
-        return null;
-    }
-
-    public ReportJobDto getAdminReport(String reportId) {
+    public ReportJobDto generateAdminReport(AdminReportCreateRequestDto reportCreateRequestDto) {
         return null;
     }
 }
