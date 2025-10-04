04a7b7f332d2f8ce8ee1b33b5a2d65ba516608b8
misiek <mbialozyt@student.agh.edu.pl>
2025-10-04 20:02:20 +0200

    feat: add jpa method for admin report

--- ApiBackend/src/main/java/com/promptoholics/anonymous/ApiBackend/domain/PensionCalculationRepository.java
+++ ApiBackend/src/main/java/com/promptoholics/anonymous/ApiBackend/domain/PensionCalculationRepository.java
@@ -1,5 +1,7 @@
 package com.promptoholics.anonymous.ApiBackend.domain;
 
+import java.time.OffsetDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -8,4 +10,6 @@ public interface PensionCalculationRepository {
     PensionCalculationEntity saveAndFlush(PensionCalculationEntity entity);
 
     Optional<PensionCalculationEntity> findById(UUID id);
+
+    List<PensionCalculationEntity> findByCreatedAtBetween(OffsetDateTime start, OffsetDateTime end);
 }
